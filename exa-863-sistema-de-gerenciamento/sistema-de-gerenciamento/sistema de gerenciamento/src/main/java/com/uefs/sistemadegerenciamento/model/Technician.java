4ea846d1748db09eb300564a246bb96919b7b6ad
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-03-11 22:00:41 -0300

    feat: add WorkOrder to Technician

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/model/Technician.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/model/Technician.java
@@ -1,11 +1,20 @@
 package com.uefs.sistemadegerenciamento.model;
 
 public class Technician extends User{
+    private WorkOrder workOrder;
     public Technician(
         String id,
         String name,
         String email
     ) {
         super(id, name, email);
+    }
+
+    public WorkOrder getWorkOrder() {
+        return workOrder;
+    }
+
+    public void setWorkOrder(WorkOrder workOrder) {
+        this.workOrder = workOrder;
     }
 }
