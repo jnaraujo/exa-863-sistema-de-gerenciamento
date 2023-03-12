2f4208bf412b3883d8db4234a96a36b44a8a467a
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-03-11 21:59:15 -0300

    feat: Technician extends User

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/model/Technician.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/model/Technician.java
@@ -1,37 +1,11 @@
 package com.uefs.sistemadegerenciamento.model;
 
-public class Technician {
-    private final String id;
-    private String name;
-    private String email;
-
+public class Technician extends User{
     public Technician(
         String id,
         String name,
         String email
     ) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+        super(id, name, email);
     }
 }
